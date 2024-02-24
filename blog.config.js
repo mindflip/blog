fe496a4f5f174baf00233a656e7d2fc0d35b04d4
mindflip <hmu1212@hotmail.com>
2024-02-24 12:07:53 +0900

    fix: set google site verification in config

--- blog.config.js
+++ blog.config.js
@@ -21,7 +21,7 @@ const BLOG = {
   socialLink: '',
   seo: {
     keywords: ['Blog', 'Website', 'Notion'],
-    googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
+    googleSiteVerification: 'NaE_dAy3L9krOCo02uyGA1rma-UYmbxd-Gjpazh56IA' // Remove the value or replace it with your own google site verification code
   },
   notionPageId: process.env.NOTION_PAGE_ID, // DO NOT CHANGE THIS！！！
   notionAccessToken: process.env.NOTION_ACCESS_TOKEN, // Useful if you prefer not to make your database public
