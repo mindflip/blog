c29b918da7ce1ee5f84f0e157a4bf2c5f612bfe2
mindflip <hmu1212@hotmail.com>
2024-02-24 14:23:07 +0900

    fix: set false index sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -4,7 +4,7 @@ module.exports = {
   siteUrl: config.link,
   generateRobotsTxt: true,
   sitemapSize: 7000,
-  generateIndexSitemap: true
+  generateIndexSitemap: false
   // ...other options
   // https://github.com/iamvishnusankar/next-sitemap#configuration-options
 }
